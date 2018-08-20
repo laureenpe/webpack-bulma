e0732e2ca1c1fbf4f7c81780e62d0beee04d082f
laura <laureen.pelaez.isaza@gmail.com>
2018-08-20 15:09:46 +0200

    updated webpack config with imagemin and others

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = env => {
 
@@ -13,7 +15,7 @@ module.exports = env => {
 
     return {
         mode: envType,
-        entry: './src/app.js',
+        entry: ['./src/app.js', 'whatwg-fetch'],
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'js/script.js'
@@ -38,33 +40,62 @@ module.exports = env => {
                 },
                 {
                     test: /\.(png|svg|jpg|gif)$/,
-                    use: ["file-loader"]
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]?[hash]',
+                            outputPath: 'images/',
+                            publicPath: isProduction ? '../images/' : ''
+                        }
+                    }]
                 },
                 {
                     test: /\.js$/,
-                    exclude: /node_modules/,
+                    exclude: "/node_modules/",
                     use: ["babel-loader"]
                 },
                 {
                     test: /\.(woff|woff2|eot|ttf|otf)$/,
-                    use: ["file-loader"]
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]?[hash]',
+                            outputPath: 'fonts/',
+                            publicPath: isProduction ? '../fonts/' : ''
+
+                        }
+                    }]
                 }
             ]
 
         },
         plugins: [
+
             new MiniCssExtractPlugin({
                 filename: "css/[name].css",
             }),
             new CleanWebpackPlugin(["dist"]),
             new HtmlWebpackPlugin({
+                filename: 'index.html',
                 template: "./src/templates/index.html",
-                title: "Bulma!"
+                title: "hello!"
             }),
             new CopyWebpackPlugin([{
                 from: "src/templates",
                 to: ""
-            }])
+            }]),
+            new ImageminPlugin({
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                disable: !isProduction, // Disable during development
+                pngquant: {
+                    quality: '80-100'
+                }
+            }),
+            new CopyWebpackPlugin([{
+                from: "src/images",
+                to: "images/[path][name].[ext]"
+            }]),
+
         ],
         optimization: {
             minimizer: [
@@ -72,8 +103,6 @@ module.exports = env => {
                     cache: true,
                     parallel: true,
                     uglifyOptions: {
-                        ie8: true,
-                        safari10: true,
                         keep_classnames: true,
                         warnings: false
                     }
